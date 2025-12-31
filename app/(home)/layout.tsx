149c322af08e4878fd0e335a70a2ddd857bd56d2
emran0335 <emranhossain0335@gmail.com>
2025-12-31 16:40:59 +0600

    database schema are written

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/home-footer";
+import Header from "@/components/home-header";
+import Navbar from "@/components/home-navbar";
+import React from "react";
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col w-full h-screen justify-between">
+      <div className="w-full h-full">
+        <Header />
+        <Navbar />
+        <div className="p-4">{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
