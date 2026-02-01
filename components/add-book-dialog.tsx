201c24b14a24b90003c8b5d33c2e6ad80babe9e1
emran0335 <emranhossain0335@gmail.com>
2026-02-01 23:37:07 +0600

    adding book data pop up is developed

--- components/add-book-dialog.tsx
+++ components/add-book-dialog.tsx
@@ -1,13 +1,38 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
 import { usePathname } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
 import { Book } from "@/app/(admin)/admin/(catalog)/columns";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { getCategories } from "@/actions/actions";
+import {
+  addBook,
+  addPhoto,
+  deletePhoto,
+  getCategories,
+  updateBook,
+} from "@/actions/actions";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Popover, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { ChevronsUpDown } from "lucide-react";
 
 type AddBookDialogProps = {
   open: boolean;
@@ -80,15 +105,157 @@ export default function AddBookDialog({
     }
   }, [book, bookForm]);
 
-  const handleItemSelect = (item: number)=> {
-    const newValue = bookForm
-  }
+  const handleItemSelect = (item: number) => {
+    const newValue = bookForm.getValues("category").slice();
+    const itemIndex = newValue.indexOf(item);
+
+    if (itemIndex === -1) {
+      newValue.push(item);
+    } else {
+      newValue.splice(itemIndex, 1);
+    }
+
+    bookForm.setValue("category", newValue);
+  };
+
+  const handleSubmit = async (values: z.infer<typeof addBookFormSchema>) => {
+    setProcessing(true);
+
+    let message = "Book added";
+
+    if (book) {
+      await updateBook({ ...values, path });
+      message = "Book updated";
+      setOpen(false);
+    } else {
+      await addBook({ ...values, path });
+    }
+
+    toast({ description: message });
+
+    bookForm.reset();
+    setProcessing(false);
+  };
+
+  const handleFileAdded = async (uploadedFiles: string[]) => {
+    if (book) {
+      const newPhoto = await addPhoto(
+        "book",
+        book.book_id,
+        uploadedFiles[0],
+        path,
+      );
+
+      if (newPhoto) {
+        book.book_photos?.push(newPhoto);
+      }
+    }
+
+    const existinPhotos = bookForm.getValues("photos");
+
+    bookForm.setValue("photos", [...existinPhotos, ...uploadedFiles]);
+  };
+
+  const handleFileDelete = async (url: string) => {
+    if (book) {
+      const photoToDelete = book.book_photos?.filter(
+        (book_photo) => book_photo.url === url,
+      );
+
+      if (photoToDelete && photoToDelete.length > 0) {
+        await deletePhoto("book", photoToDelete[0].photo_id, path);
+      }
+    }
+
+    const updatedPhoto =
+      bookForm.getValues("photos").filter((book_photo) => book_photo !== url) ??
+      [];
+
+    bookForm.setValue("photos", updatedPhoto);
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add book</DialogTitle>
+          <DialogDescription></DialogDescription>
+          <Form {...bookForm}>
+            <form onSubmit={bookForm.handleSubmit(handleSubmit)}>
+              <FormField
+                control={bookForm.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Book Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="book name" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={bookForm.control}
+                name="author"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Author</FormLabel>
+                    <FormControl>
+                      <Input placeholder="book name" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={bookForm.control}
+                name="isbn"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>ISBN</FormLabel>
+                    <FormControl>
+                      <Input placeholder="XXX-X-XX-XXXXXX-X" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={bookForm.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <Popover>
+                      <PopoverTrigger>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            role="combobox"
+                            className={cn(
+                              "justify-between",
+                              !field.value && "text-muted-foreground",
+                            )}
+                          >
+                            {field.value.length > 0
+                              ? field.value
+                                  .map(
+                                    (val) =>
+                                      categories.find(
+                                        (c) => c.category_id === val,
+                                      )?.category_name,
+                                  )
+                                  .join("")
+                              : "Select a category"}
+                              <ChevronsUpDown className="opacity-50"/>
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                    </Popover>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </DialogHeader>
       </DialogContent>
     </Dialog>
